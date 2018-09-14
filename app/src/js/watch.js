5f6c6198d808bc782e723b335fbdfd8eee7deb46
fullnelson14 <bradjnelson14@gmail.com>
2018-09-13 21:39:22 -0600

    groundwork for homepage layout. links implemented.

--- app/src/js/watch.js
+++ app/src/js/watch.js
@@ -1,7 +1,7 @@
 import "../pug/index.pug";
+import "../pug/projects.pug";
+import "../pug/portfolio.pug";
+import "../pug/info.pug";
 
 import "../pug/components/header.pug";
 import "../pug/components/footer.pug";
-import "../pug/components/info.pug";
-import "../pug/components/projects.pug";
-import "../pug/components/portfolio.pug";
